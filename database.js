f1873ad9f52c47744e44d2024507a8d0cff7a49c
len-lab393 <emperorlenj@gmail.com>
2026-02-17 20:49:33 +0300

    Delete database.js

--- database.js
+++
@@ -1,8 +0,0 @@
-const sqlite3 = require("sqlite3").verbose();
-
-const db = new sqlite3.Database("./database.db", (err) => {
-  if (err) console.error(err);
-  else console.log("Database connected");
-});
-
-module.exports = db;
