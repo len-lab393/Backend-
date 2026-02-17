992a521b3c869066f7d2ebf2425f1211de161bda
len-lab393 <emperorlenj@gmail.com>
2026-02-17 16:03:50 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 const express = require("express");
+const { Pool } = require("pg");
+
 const app = express();
 
-app.use(express.json());
-
-app.get("/", (req, res) => {
-  res.send("Backend running");
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL
 });
 
-app.listen(3000, () => {
-  console.log("Server running");
+app.get("/users", async (req, res) => {
+  const result = await pool.query("SELECT NOW()");
+  res.json(result.rows);
 });
+
+app.listen(3000);
