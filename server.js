a20b349ca92f9f5536eb779d6c81a6e3b0f436c8
len-lab393 <emperorlenj@gmail.com>
2026-02-22 01:45:42 +0300

    Update server.js

--- server.js
+++ server.js
@@ -18,3 +18,5 @@ const PORT = process.env.PORT || 3000
 app.listen(PORT, ()=>{
 console.log("Server running on port " + PORT)
 })
+app.use("/escort", require("./routes/escort"));
+app.use("/uploads", express.static("uploads"));
