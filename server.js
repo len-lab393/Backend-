0cc4aac99632b599877f8a19f969b77ddb7a0f58
len-lab393 <emperorlenj@gmail.com>
2026-02-24 20:59:10 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,20 +1,12 @@
+require("dotenv").config();
 const express = require("express");
 
 const app = express();
-const PORT = process.env.PORT || 3000;
-
 app.use(express.json());
 
-// health check
 app.get("/", (req, res) => {
-  res.send("BACKEND LIVE 🚀");
+  res.send("Server running");
 });
 
-// test API route
-app.get("/api/test", (req, res) => {
-  res.json({ message: "API working" });
-});
-
-app.listen(PORT, () => {
-  console.log("Server running on port " + PORT);
-});
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log("Server started"));
