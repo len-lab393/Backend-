1e476517c80b24a535e12cf7baa73995750e498b
len-lab393 <emperorlenj@gmail.com>
2026-02-24 20:44:11 +0300

    Update server.js

--- server.js
+++ server.js
@@ -5,8 +5,14 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
+// health check
 app.get("/", (req, res) => {
   res.send("BACKEND LIVE 🚀");
+});
+
+// test API route
+app.get("/api/test", (req, res) => {
+  res.json({ message: "API working" });
 });
 
 app.listen(PORT, () => {
