f91eff341f9db5098bcb49c0ec505b007167fa4d
len-lab393 <emperorlenj@gmail.com>
2026-02-24 20:20:30 +0300

    Update server.js

--- server.js
+++ server.js
@@ -3,8 +3,10 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
-  res.send("BACKEND WORKING");
+  res.send("BACKEND LIVE 🚀");
 });
 
 app.listen(PORT, () => {
