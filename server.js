8a2d74ea347c2ec9395132d81ee315861c5ab386
len-lab393 <emperorlenj@gmail.com>
2026-02-24 20:00:50 +0300

    Update server.js

--- server.js
+++ server.js
@@ -3,21 +3,8 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// middleware
-app.use(express.json());
-
-// health check route
 app.get("/", (req, res) => {
-  res.send("BACKEND WORKING 🚀");
-});
-
-// prevent crash from unknown errors
-process.on("uncaughtException", err => {
-  console.error("CRASH:", err);
-});
-
-process.on("unhandledRejection", err => {
-  console.error("PROMISE ERROR:", err);
+  res.send("BACKEND WORKING");
 });
 
 app.listen(PORT, () => {
