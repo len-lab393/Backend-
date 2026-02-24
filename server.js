74255c0b5b86816d0a79895ba115035bfc460234
len-lab393 <emperorlenj@gmail.com>
2026-02-24 12:36:04 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req,res)=>res.send("WORKING"));
+const PORT = process.env.PORT || 3000;
 
-app.listen(process.env.PORT || 3000, "0.0.0.0", () => {
-  console.log("Server running");
+app.get("/", (req, res) => res.send("WORKING"));
+
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
 });
