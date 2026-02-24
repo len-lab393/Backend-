e6b9ba4159db022ca4bb65d6280d420fc84bdb4a
len-lab393 <emperorlenj@gmail.com>
2026-02-24 21:02:55 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,27 @@
 require("dotenv").config();
+
 const express = require("express");
 
 const app = express();
-app.use(express.json());
 
+// middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// test route
 app.get("/", (req, res) => {
-  res.send("Server running");
+  res.send("Server running successfully");
 });
 
+// callback route (needed later for MPesa)
+app.post("/callback", (req, res) => {
+  console.log("Callback received:", req.body);
+  res.sendStatus(200);
+});
+
+// start server
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log("Server started"));
+
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
