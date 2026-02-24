1071e5d555c8fd65acc34925aa34e613edfb6d8d
len-lab393 <emperorlenj@gmail.com>
2026-02-25 00:38:30 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,27 +1,27 @@
 require("dotenv").config();
-
 const express = require("express");
+const { stkPush } = require("./mpesa");
 
 const app = express();
-
-// middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // test route
-app.get("/", (req, res) => {
-  res.send("Server running successfully");
+app.get("/", (req, res) => res.send("Server running successfully"));
+
+// STK push route
+app.post("/pay", (req, res) => {
+  const { phone, amount } = req.body;
+  stkPush(phone, amount)
+    .then(response => res.json(response.data))
+    .catch(err => res.status(500).json({ error: err.message }));
 });
 
-// callback route (needed later for MPesa)
+// callback
 app.post("/callback", (req, res) => {
   console.log("Callback received:", req.body);
   res.sendStatus(200);
 });
 
-// start server
 const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-  console.log(`Server started on port ${PORT}`);
-});
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
