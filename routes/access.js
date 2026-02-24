c5b710fbcaa01e854dda01f9290160b555000260
len-lab393 <emperorlenj@gmail.com>
2026-02-24 20:12:51 +0300

    Delete routes directory

--- routes/access.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-router.get("/check-access/:phone", (req, res) => {
-  res.json({ success: true });
-});
-
-module.exports = router;
