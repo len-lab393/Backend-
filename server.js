f8dd285f890547cb200c27d4421b7cea047f86f4
len-lab393 <emperorlenj@gmail.com>
2026-03-01 01:02:30 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 require("dotenv").config()
 
-const express = require("express")
-const cors = require("cors")
-const { MongoClient, ObjectId } = require("mongodb")
+const mongoose = require("mongoose")
+
+mongoose.connect(process.env.MONGO_URI)
 
 const app = express()
 
